f682a2c04396171c282ea425ee51afbe37ad1453
NicErGoblinQuelloVero <105206534+NicErGoblinQuelloVero@users.noreply.github.com>
2023-10-06 22:21:01 +0200

    Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,40 +1,47 @@
 import xeqLogo from './assets/xeqLogo.png'
 const Footer = () => {
     return (
-        <footer className="bg-light py-4 mt-auto">
-        <div className="container px-4">
+        <div  style={{overflow:'hidden'}}>
+            <footer className=" bg-light py-4 mt-auto row row-cols-1 row-cols-sm-2 row-cols-md-5 ">
+             
+                <div className="col mb-3 socialFooter" style={{textAlign:'left',marginLeft:"80px"}} >
 
-          
-            <div className="row align-items-center justify-content-between flex-column flex-sm-row">
-
-                <div className="d-flex fs-2 gap-3 mb-4">
-                    <img src={xeqLogo} style={{width:'80px'}}></img>
-                </div>
-                <div className="d-flex fs-2 gap-3">
-                    <h1>Follow Us</h1>
-                </div>
-                
-            
-                <div className="d-flex fs-2 gap-3">
+                    <img src={xeqLogo} style={{width:"50px"}}/>
+                    <h3>Follow Us</h3>
+                    <div className="d-flex fs-2 gap-3">
                     <a className="text-black" href="#!"><i class="bi bi-telegram"></i></a>
                     <a className="text-black" href="#!"><i class="bi bi-youtube"></i></a>
                     <a className="text-black" href="#!"><i class="bi bi-reddit"></i></a>
                     <a className="text-black" href="#!"><i class="bi bi-x"></i></a>
                 </div>
-                <div className="d-flex gap-3">
-                    
-
-                <div className="small m-0">Copyright &copy; Equilibria2023</div>
                 </div>
-               
-            </div>
-            
-           <div className="col">
+                <div className="col mb-3" style={{textAlign:'left',marginLeft:"20px"}} >
+                    <h5>Info</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Announcements</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Coinmarketcap</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Coingecko</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Explorer</a></li>
+                    </ul>
+                </div>
 
-           </div>
-        </div>
+                <div className="col mb-3"  style={{textAlign:'left',marginLeft:"20px"}} >
+                    <h5>Tools</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Desktop Wallet</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Media Kit</a></li>
+                    </ul>
+                </div>
+
+                <div className="col mb-3"  style={{textAlign:'left',marginLeft:"20px"}} >
+                <h5>Pools</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Miningpoolstats</a></li>
+                    </ul>
+                </div>
+        </footer>
+  </div>
      
-    </footer>
     );
 }
  
